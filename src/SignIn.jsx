a4611604043e5c0da9b0cf47f2193a5000e29c28
MACKOzKLANU <sek-maciek@o2.pl>
2024-12-27 21:44:26 +0100

    not finished signup/in

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,22 +1,68 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { doSignInWithEmailAndPassword, doSignInWithGoogle } from './firebase/auth';
+import { useAuth } from './contexts/authContext';
 
 function SignIn() {
+    const { userLoggedIn } = useAuth();
+
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isSigningIn, setIssigningIn] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleSubmit = (event) => {
+    // useEffect(() => {
+    //     const ui = new firebaseui.auth.AuthUI(firebase.auth());
+    //     ui.start('#firebaseui-auth-container', {
+    //         signInOptions: [
+    //             firebase.auth.EmailAuthProvider.PROVIDER_ID
+    //         ],
+    //         // Other config options...
+    //     });
+    // }, []);
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
         // Handle sign-in logic here
+        if(!isSigningIn){
+            setIssigningIn(true);
+            await doSignInWithEmailAndPassword(email, password)
+
+        }    
+
         console.log('Email:', email);
         console.log('Password:', password);
     };
 
+    const handleGoogleSignIn = (event) => {
+        event.preventDefault();
+        // Handle Google sign-in logic here
+        if(!isSigningIn){
+            setIssigningIn(true);
+            doSignInWithGoogle().catch(err => {
+                setIssigningIn(false);
+            })
+        }
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
+            {userLoggedIn && (<Navigate to={"/home"} replace={true} />)}
             <div className="card p-4" style={{ width: '100%', maxWidth: '400px' }}>
                 <h2 className="text-center mb-4">Sign In</h2>
                 <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name:</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            placeholder="Enter your name"
+                            required
+                        />
+                    </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email:</label>
                         <input
@@ -41,8 +87,7 @@ function SignIn() {
                     </div>
                     <button type="submit" className="btn btn-dark w-100">Sign In</button>
                     <div className="text-center mt-3">
-
-                        <Link to="/signup">Don't have an account? Sign up</Link>
+                        <Link to="/signup">Don't have and account? sign up</Link>
                     </div>
                 </form>
             </div>
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,14 +1,23 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { doCreateUserWIthEmailAndPassword } from './firebase/auth';
 
 function SignUp() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
         // Handle sign-up logic here
+        if(!isRegistering){
+            setIsRegistering(true);
+            await doCreateUserWIthEmailAndPassword(email, password)
+        }
+
         console.log('Name:', name);
         console.log('Email:', email);
         console.log('Password:', password);
@@ -16,6 +25,8 @@ function SignUp() {
 
     return (
         <div className="d-flex justify-content-center align-items-center vh-100">
+            {userLoggedIn && (<Navigate to={"/home"} replace={true} />)}
+            
             <div className="card p-4" style={{ width: '100%', maxWidth: '400px' }}>
                 <h2 className="text-center mb-4">Sign Up</h2>
                 <form onSubmit={handleSubmit}>
